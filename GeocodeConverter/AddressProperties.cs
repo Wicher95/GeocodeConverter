50e2b8b2345aa28372d9995b5c8739b29fe4f7ad
Wicher95 <wicher.1995@gmail.com>
2018-04-03 16:37:15 +0200

    x

--- GeocodeConverter/AddressProperties.cs
+++ GeocodeConverter/AddressProperties.cs
@@ -8,6 +8,7 @@
         private string name;
         private string type;
         private string category;
+        private string openTime;
 
         public string StrNumber { get => strNumber; set => strNumber = value; }
         public string StrName { get => strName; set => strName = value; }
@@ -15,6 +16,7 @@
         public string Name { get => name; set => name = value; }
         public string Type { get => type; set => type = value; }
         public string Category { get => category; set => category = value; }
+        public string OpenTime { get => openTime; set => openTime = value; }
     }
 
 }
